f9e423334dc0bc1a6475e89049a7bf74e5c6f5a8
Roger Melo <rogerw592@gmail.com>
2020-03-22 22:03:02 -0300

    Add 'exercicio-15' challenges

---
+++ exercicio-15/app.js
@@ -0,0 +1,68 @@
+/*
+  01
+
+  - Obtenha a ul do index.html e adicione em todos os elementos filhos dela,  
+    uma classe 'video';
+  - Exiba no console os elementos filhos da ul com a classe já inserida.
+*/
+
+
+
+/*
+  02
+
+  - Usando a propriedade adequada, descubra quem é o elemento pai do h2
+    e exiba-o no console;
+*/
+
+
+
+/*
+  03
+
+  - Descubra quem é o próximo elemento irmão do h1 e exiba-o no console;
+*/
+
+
+
+/*
+  04
+
+  - Descubra quem é o irmão anterior da ul e exiba-o no console;
+*/
+
+
+
+/*
+  05
+
+  - Quando um clique acontecer em alguma das lis, faça com que a li clicada seja  
+    exibida no console.
+*/
+
+
+
+/*
+  06
+
+  - Quando o botão for clicado, adicione o nome dos vídeos abaixo dentro da ul;
+  - Cada nome deve estar dentro de uma li.
+*/
+
+const videos = [{
+  name: 'Como o promise all funciona | JavaScript',
+  length: '00:01:52'
+}, {
+  name: 'Como refatorar um for loop | JavaScript',
+  length: '00:04:18'
+}, {
+  name: 'Como fazer requisições HTTP com o método fetch | JavaScript',
+  length: '00:02:55'
+}]
+
+/*
+  07
+
+  - Se um clique no h1 acontecer, faça com que todos os elementos dentro do body 
+    sejam removidos.
+*/
