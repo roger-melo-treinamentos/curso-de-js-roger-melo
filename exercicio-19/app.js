770c16918e763db45efe66f40e993e026daa1d79
Roger Melo <rogerw592@gmail.com>
2020-04-27 17:57:21 -0300

    Add 'exercicio-19' challenges

---
+++ exercicio-19/app.js
@@ -0,0 +1,21 @@
+/*
+Este exercício será um pouquinho diferente dos anteriores.
+
+Seu desafio é desenvolver uma versão do quiz que:
+
+- Aborda um tema diferente (não pode ser de filmes);
+- Tem um tema de cores diferente do que foi apresentado na aula;
+- Exibe na tela a pontuação que o usuário fez. Não há certo ou errado, apenas faça. Essa exibição de pontos é uma das implementações que faremos na próxima aula =D
+
+Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, tente fazer esse exercício sem rever partes da aula.
+
+Depois de fazer o que foi pedido acima, crie um repositório no GitHub para a sua aplicação e abra uma issue no repositório do curso com:
+
+- O link da sua versão do quiz;
+- Quais foram as suas maiores dúvidas ou dificuldades durante a execução desse exercício;
+- Quais foram as suas menores dificuldades durante a execução desse exercício.
+
+Link do repositório do curso: https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo/issues
+
+Ps: se você não conseguiu fazer tudo o que foi pedido acima, abra a issue mesmo assim =)
+*/
