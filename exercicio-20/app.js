deedf606d5fdf1e3ad9178512d2f63fe5ef1bed6
Roger Melo <rogerw592@gmail.com>
2020-04-27 19:12:08 -0300

    Add 'exercicio-20' challenges

---
+++ exercicio-20/app.js
@@ -0,0 +1,69 @@
+/* 
+  01
+
+  - Insira apenas o css do bootstrap no index.html.
+*/
+
+/* 
+  02
+
+  - Após um segundo e meio do carregamento da página, exiba a mensagem  
+    "Um segundo e meio se passaram desde que a página foi carregada".
+*/
+
+
+
+/* 
+  03
+
+  - Faça o contador do index.html funcionar;
+  - O clique no botão "Parar contador" deve fazer com que o contador exiba 0.
+*/
+
+
+
+/* 
+  04
+
+  - A partir deste exercício, vamos voltar a trabalhar na sua versão do Quiz =D
+  - Adicione mais 2 alternativas em cada pergunta do quiz.
+*/
+
+/* 
+  05
+
+  - Modifique a ordem das alternativas corretas. Ou seja, faça com que a  
+    alternativa correta das perguntas não seja apenas a alternativa "B".
+*/
+
+/* 
+  06
+
+  - Refatore o código.
+  
+  Dicas: 
+    - Primeiro, quebre o código da função de callback de envio do form em  
+      funções isoladas;
+    - Depois, cuide da legibilidade das funções.
+*/
+
+/* 
+  07
+
+  O exercício agora é considerarmos fazer dessa aplicação uma peça do seu 
+  portfólio.
+
+  Se você conseguiu executar o exercício da aula passada, especialmente sem ter  
+  que rever partes da aula, ou seja, se você sente que desenvolveu a sua versão  
+  do quiz por conta própria, considere inserí-la como parte de seu portfólio.
+
+  Caso contrário, reveja as aulas e treine novamente até que você consiga 
+  desenvolver a aplicação sozinho(a).
+
+  A ideia é: considere inserir a sua versão do quiz em seu portifólio apenas se 
+  você sente que *aprendeu* a desenvolvê-la.
+
+  Se você ainda não tem um site próprio para hospedar os arquivos da aplicação, 
+  você pode hospedá-la no Netlify seguindo este tutorial: 
+  https://www.youtube.com/playlist?list=PLlAbYrWSYTiMGMxQf9JSoZUU1rgVpGPth
+*/
