be524dddb1711611f970a3ee0f0f4c6c4c9fd4a7
Roger-Melo <rogerw592@gmail.com>
2023-08-01 23:11:37 -0300

    add css observation

--- exercicio-19/app.js
+++ exercicio-19/app.js
@@ -5,9 +5,18 @@ Seu desafio é desenvolver uma versão do quiz que:
 
 - Aborda um tema diferente (não pode ser de filmes);
 - Tem um tema de cores diferente do que foi apresentado na aula;
-- Exibe na tela a pontuação que o usuário fez. Não há certo ou errado, apenas faça. Essa exibição de pontos é uma das implementações que faremos na próxima aula =D
+- Exibe na tela a pontuação que o usuário fez. Não há certo ou errado, 
+apenas faça. Essa exibição de pontos é uma das implementações que faremos 
+na próxima aula =D
 
-Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, busque fazer esse exercício sem rever partes da aula.
+Independente se você já fez o quiz dos filmes enquanto acompanhava a aula, 
+busque fazer esse exercício sem rever partes da aula.
 
-É importante que a sua versão do quiz seja feita apenas com o conteúdo que vimos até aqui.
+É importante que a sua versão do quiz seja feita apenas com o conteúdo que 
+vimos até aqui.
+
+O uso do Bootstrap ou qualquer biblioteca CSS é opcional. Porém, eu recomendo 
+que nesse momento você foque em desenvolver a lógica da aplicação com o 
+JavaScript. Em um momento futuro, você pode voltar e melhorar a estilização 
+da aplicação. 
 */
